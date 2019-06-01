832e918c72606c8eb9e16cae424e21d602a950f9
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-06-01 13:03:25 +0800

    Implement proxyUtil

---
+++ util/proxyUtil.js
@@ -0,0 +1,32 @@
+const { L0, L1, L2 } = require('../static/constants')
+const _ = require('lodash')
+const apexApiSigningUtil = require('./apexApiSigningUtil')
+const config = require('../config/index')
+const loggingService = require('../services/loggingService');
+
+const APEX_INTERNET_POLICY = config.env('APEX_INTERNET_POLICY', config.get('apex.internet.policy'))
+const APEX_INTRANET_POLICY = config.env('APEX_INTRANET_POLICY', config.get('apex.intranet.policy'))
+
+module.exports = class proxyUtil {
+  static setAuthorizationHeader(proxyReq, req, res, options) {
+    loggingService.info(`APEX internet policy: ${APEX_INTERNET_POLICY}; APEX intranet policy: ${APEX_INTRANET_POLICY}`)
+
+    if (APEX_INTERNET_POLICY === L0 && APEX_INTRANET_POLICY === L0) return this
+
+    return proxyReq.setHeader('Authorization', this.buildApexAuthorizationHeader(req.method, req.url));
+  }
+
+  static buildApexAuthorizationHeader(method, url) {
+    let header = []
+
+    if (APEX_INTRANET_POLICY === L1 || APEX_INTRANET_POLICY === L2) {
+      header.push(apexApiSigningUtil.generateApexIntranetHeader(method, url))
+    }
+
+    if (APEX_INTERNET_POLICY === L1 || APEX_INTERNET_POLICY === L2) {
+      header.push(apexApiSigningUtil.generateApexInternetHeader(method, url))
+    }
+
+    return _.join(header, ', ')
+  }
+}
