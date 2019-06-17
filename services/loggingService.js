ac7c35378732404ac73d95a065788ad71c9b0b7a
Gerald Khoo <khoo0030@e.ntu.edu.sg>
2019-06-17 14:59:48 +0800

    Format loggingService log formats

--- services/loggingService.js
+++ services/loggingService.js
@@ -1,23 +1,51 @@
 const config = require('./../config/index')
+const path = require('path')
 const winston = require('winston')
 
-const NODE_ENV = config.env('NODE_ENV', config.get('app.env', 'development'))
+const NODE_ENV = config.env('NODE_ENV', config.get('app.env'))
 const LOGGING_LEVEL = config.env('LOGGING_LEVEL', config.get('app.logging.level', NODE_ENV === 'production' ? 'debug' : 'info'))
 
 const format = winston.format
 
+const options = {
+  file: {
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.metadata({fillExcept: ['level', 'label', 'timestamp', 'message']}),
+      format.json()
+    ),
+    filename: path.resolve(process.cwd(), 'logs', 'winston.log'),
+    handleExceptions: true,
+    json: true,
+    colorize: false,
+  },
+  console: {
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.simple(),
+    ),
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+}
+
 const loggingService = winston.createLogger({
-  level: LOGGING_LEVEL,
-  format: winston.format.json(),
+  format: format.combine(
+    format.timestamp(),
+  ),
   transports: [
-    new winston.transports.Console({
-      format: format.combine(
-        format.timestamp(),
-        format.colorize(),
-        format.simple()
-      ),
-    })
-  ]
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false,
 })
+
+loggingService.stream = {
+  // eslint-disable-next-line no-unused-vars
+  write: function(message, encoding) {
+    loggingService.info(message)
+  },
+}
 
 module.exports = loggingService
